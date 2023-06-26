5dfa81b0cbdbb909eb5d5c675e13e2f73d8fbc75
Kenpachi2 <ifeanyiumeadi201@gmail.com>
2023-06-26 06:12:03 -0700

    push tasks with readme

---
+++ tasks/task7.js
@@ -0,0 +1,28 @@
+
+let dayOfWeek = "Tuesday";
+
+switch (dayOfWeek) {
+  case "Monday":
+    console.log("Monday: Another week to grind!");
+    break;
+  case "Tuesday":
+    console.log("Tuesday: Life na jeje sha!!");
+    break;
+  case "Wednesday":
+    console.log("Wednesday: Halfway through the week, God help us.");
+    break;
+  case "Thursday":
+    console.log("Thursday: Almost there.");
+    break;
+  case "Friday":
+    console.log("Friday: TGIF, na club get us today.");
+    break;
+  case "Saturday":
+    console.log("Saturday: Weekend vibes");
+    break;
+  case "Sunday":
+    console.log("Sunday: Relax and chill my bro");
+    break;
+  default:
+    console.log("Invalid day of the week");
+}
